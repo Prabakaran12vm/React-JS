171400ee110006d22f51c9782367df17f45bb6aa
Prabakaran VM <prabakaran12vm@gmail.com>
2024-06-18 11:55:10 +0530

    router, about and contact added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react"
 import ReactDOM  from "react-dom/client"
 import Header from './components/Header.js'
 import Body from './components/Body.js'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import About from "./components/About.js"
+import Contact from "./components/Contact.js"
 
 const AppLayout = ()=>{
    return (
@@ -10,5 +13,23 @@ const AppLayout = ()=>{
     <Body/>
     </div>)
 }
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element : <AppLayout/>
+
+    },
+    {
+        path:"/about",
+        element: <About/>
+    }, 
+    {
+        path:"/contact",
+        element: <Contact/>
+
+    }
+    
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+root.render(<RouterProvider router={appRouter}/> )
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact  = ()=>{
+    return (
+        <div>
+            <h1>Buy me a Coffee</h1>
+            <h2>Contact via linkedin, facebook</h2>
+        </div>
+    )
+}
+export default Contact
