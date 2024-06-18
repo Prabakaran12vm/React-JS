b3b84586a6a4b0974220edc4bbbcce5d309f0cd2
Prabakaran VM <prabakaran12vm@gmail.com>
2024-06-18 21:14:38 +0530

    links for pages added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,38 @@
-import { useEffect, useState } from 'react'
-import logo from '../../images/logo.png'
-const Header = ()=>{
-    const[loginBtn,setauth]  =useState("Login") 
-    useEffect(()=>console.log("Use Effect is called"))
-    return (
-    
+import { useEffect, useState } from "react";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+const Header = () => {
+  const [loginBtn, setauth] = useState("Login");
+  useEffect(() => console.log("Use Effect is called"));
+  return (
     <div className="header">
-        <div className="logo-container">
-            <img className="logo"
-            src={logo}
-            />
-        </div>
-        <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact</li>
-                <li>Cart</li>
-          
-            <button className='login' onClick={()=>{
-            loginBtn ==="Login"
-            ? setauth("Logout")
-            : setauth("Login");
-        }}>{loginBtn}</button>
-          </ul>
-        </div>
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li> Cart</li>
+
+          <button
+            className="login"
+            onClick={() => {
+              loginBtn === "Login" ? setauth("Logout") : setauth("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
+        </ul>
+      </div>
     </div>
-)
-}
+  );
+};
 export default Header;
