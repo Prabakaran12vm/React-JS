3f11a0e196e25c52334d8efbd4b237d8ca8fa2b1
Prabakaran VM <prabakaran12vm@gmail.com>
2024-07-02 11:07:50 +0530

    userestaurantdata custom hook created

---
+++ src/utils/useRestaurantData.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { FOOD_API } from "./constants";
+const useRestaurantData = () => {
+  const [listOfRes, setListofRes] = useState([]);
+  const [filteredRes, setFilteredRes] = useState(listOfRes);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(FOOD_API);
+    const json = await data.json();
+    setListofRes(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRes(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log("This is PBK")
+  };
+  return [listOfRes, filteredRes];
+};
+export default useRestaurantData;
