da8b1b6a2209bad20d64cc913de5d3ca6f8b00cd
Prabakaran VM <prabakaran12vm@gmail.com>
2024-07-02 11:06:49 +0530

    css changes in body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import RestaurantCard from "./Restaurantcard";
+import RestaurantCard from "./RestaurantCard.js";
 import Shimmer from "./shimmerUI";
 import { FOOD_API } from "../utils/constants";
 import { Link } from "react-router-dom";
@@ -13,11 +13,9 @@ const Body = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  
+
   const fetchData = async () => {
-    const data = await fetch(
-      FOOD_API
-       );
+    const data = await fetch(FOOD_API);
     const json = await data.json();
     // console.log(json)
     // optional chaining
@@ -29,59 +27,63 @@ const Body = () => {
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
     // data.cards[1].card.card.gridElements.infoWithStyle.restaurants
-    
   };
   const onlineStatus = useOnlineStatus();
-  if (onlineStatus ===false){
-    return <h1>Looks like offline!</h1>
+  if (onlineStatus === false) {
+    return <h1>Looks like offline!</h1>;
   }
-  
+
   return list0fRes.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter">
-        <div className="search">
+      <div className="filter flex">
+        <div className="search m-4 p-4 ">
           <input
             type="text"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
-             
-              setSearchText(e.target.value)
+              setSearchText(e.target.value);
 
               const filteredRes = list0fRes.filter((res) =>
                 res.info.name.toLowerCase().includes(searchText)
               );
               setFilteredRes(filteredRes);
             }}
-            className="search-box"
           />
-          <button onClick={() => {setFilteredRes(filteredRes);}}>Search</button>
+          <button
+            className="px-4 py-2 m-4 bg-gray-200 shadow-lg"
+            onClick={() => {
+              setFilteredRes(filteredRes);
+            }}
+          >
+            Search
+          </button>
         </div>
-
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const topRes = list0fRes.filter((res) => res.info.avgRating > 4);
-            setFilteredRes(topRes);
-
-          }}
-        >
-          Top Restaurants
-        </button>
+        <div className="search m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-200 rounded-lg"
+            onClick={() => {
+              const topRes = list0fRes.filter((res) => res.info.avgRating > 4);
+              setFilteredRes(topRes);
+            }}
+          >
+            Top Restaurants
+          </button>
+        </div>
       </div>
 
-      <div className="res-container">
-        
+      <div className="ml-[50px] flex flex-wrap shadow-sm">
         {filteredRes.map((restaurant) => (
-          <Link key={restaurant.info.id}  to={"/restaurant/" + restaurant.info.id}>
-          <RestaurantCard 
-          
-           resData={restaurant} />
+          <Link
+            key={restaurant.info.id}
+            to={"/restaurant/" + restaurant.info.id}
+          >
+            <RestaurantCard resData={restaurant} />
           </Link>
-        ))}{
-
-        }
+        ))}
+        {}
       </div>
     </div>
   );
