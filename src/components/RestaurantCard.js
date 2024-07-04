1597fb0810736936aa24f8cfa9e324f491a09ce4
Prabakaran VM <prabakaran12vm@gmail.com>
2024-07-04 21:24:55 +0530

    css changes in restaurantcard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,16 +5,16 @@ const RestaurantCard = (props) => {
   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
     resData?.info; // Destructuring
   return (
-    <div className="m-4 p-4 w-[200px] shadow-lg font-sans " >
+    <div className="m-4 p-4 w-[200px] h-[350px]  shadow-xl font-sans rounded-md   hover:scale-[0.99]" >
       <img
-        className="rounded-lg w-[200px] h-[150px]"
+        className="rounded-lg w-[220px] h-[138.5px] "
         alt="reslogo"
         src={CDN_URL + cloudinaryImageId}
       />
       
       <h3 className="font-bold text-gray-700">{name}</h3>
       <h4 className="text-gray-600">{cuisines.join(", ")}</h4>
-      <h4 className="">{avgRating}Stars</h4>
+      <h4 className="text-gray-600">{avgRating}Stars</h4>
       <h4 className="text-gray-600">{costForTwo}For Two</h4>
       
     </div>
