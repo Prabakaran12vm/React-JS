0c07add0daf769d71c49472bdb577bde5bb7c531
Prabakaran VM <prabakaran12vm@gmail.com>
2024-07-21 22:56:21 +0530

    react context

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useContext } from "react";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
+import UserContext from "../utils/UserContext";
 const Header = () => {
   const [loginBtn, setauth] = useState("Login");
-
+  const data = useContext(UserContext)
+  console.log(data)
   return (
     <div className="flex justify-between shadow-md   text-gray-600 font-semibold">
       <div className="logo-container ml-2">
         <Link to={"/"}><img className="w-20 hover:scale-[1.1]" src={logo} /></Link> 
       </div>
-      <div className="flex items-center ">
+      <div className="flex items-center">
         <ul className="flex p-4 m-4 ">
           <li className="px-4 hover:text-orange-500">
             <Link to="/">Home</Link>
@@ -32,6 +34,7 @@ const Header = () => {
           >
             {loginBtn}
           </button>
+          <li className="px-4 text-green-600  hover:text-orange-500">{data.loggedInUser}</li>
         </ul>
       </div>
     </div>
