3dcd29e01b8fae87649592ca3e98ea7f6513e3db
Prabakaran VM <prabakaran12vm@gmail.com>
2024-08-07 06:49:46 +0530

    redux store subscribing in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import { useEffect, useState,useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 const Header = () => {
   const [loginBtn, setauth] = useState("Login");
-  const data = useContext(UserContext)
-  console.log(data)
+  const data = useContext(UserContext);
+  // selector for subscribing
+  const cartItem = useSelector((store)=>store.cart.items) 
+
   return (
     <div className="flex justify-between shadow-md   text-gray-600 font-semibold">
       <div className="logo-container ml-2">
-        <Link to={"/"}><img className="w-20 hover:scale-[1.1]" src={logo} /></Link> 
+        <Link to={"/"}>
+          <img className="w-20 hover:scale-[1.1]" src={logo} />
+        </Link>
       </div>
       <div className="flex items-center">
         <ul className="flex p-4 m-4 ">
@@ -20,7 +25,7 @@ const Header = () => {
             <Link to="/about">About</Link>
           </li>
           <li className="px-4 hover:text-orange-500">
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact">Cart - ({cartItem.length})</Link>
           </li>
           <li className="px-4 hover:text-orange-500">
             <Link to="/grocery">Grocery</Link>
@@ -34,7 +39,9 @@ const Header = () => {
           >
             {loginBtn}
           </button>
-          <li className="px-4 text-green-600  hover:text-orange-500">{data.loggedInUser}</li>
+          <li className="px-4 text-green-600  hover:text-orange-500">
+            {data.loggedInUser}
+          </li>
         </ul>
       </div>
     </div>
