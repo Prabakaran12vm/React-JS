990826d14c782c4a0ac96097b08bbc703bd8e86e
Prabakaran VM <prabakaran12vm@gmail.com>
2024-08-17 21:42:50 +0530

    contact page testing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,11 @@ const Contact  = ()=>{
     return (
         <div>
             <h1>Buy me a Coffee</h1>
-            <h2>Contact via linkedin, facebook</h2>
+            <div>
+            <h1>Contact via linkedin, facebook</h1>
+            </div>
+            {/* <input className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300">Name</input> */}
+        <button className="text-white bg-blue-700 hover:bg-blue-800">button</button>
         </div>
     )
 }
---
+++ src/components/__test__/Contact.test.js
@@ -0,0 +1,22 @@
+import Contact from "../Contact"
+import "@testing-library/jest-dom"
+import { render,screen } from "@testing-library/react";
+
+test ("should load Contact Us component", ()=>{
+    render(<Contact/>);
+
+    const button = screen.getByText("button")
+    
+    // Assertion
+    expect(button).toBeInTheDocument();
+
+})
+test ("should load two h1 tags", ()=>{
+    render(<Contact/>)
+    const heading = screen.getByRole("button")
+    // Assertion
+    expect(heading).toBeInTheDocument()
+    
+})
+
+
