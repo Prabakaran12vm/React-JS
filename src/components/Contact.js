949ed10b2b377f5245a4dbb4bd94ec3856c7046a
Prabakaran VM <prabakaran12vm@gmail.com>
2024-08-19 22:08:29 +0530

    contact comp testing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,19 @@
-const Contact  = ()=>{
-    return (
-        <div>
-            <h1>Buy me a Coffee</h1>
-            <div>
-            <h1>Contact via linkedin, facebook</h1>
-            </div>
-            {/* <input className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300">Name</input> */}
-        <button className="text-white bg-blue-700 hover:bg-blue-800">button</button>
-        </div>
-    )
-}
-export default Contact
+const Contact = () => {
+  return (
+    <div>
+      <h1>Buy me a Coffee</h1>
+      <div>
+        <h1>Contact via linkedin, facebook</h1>
+      </div>
+      
+      <div>
+      <label>
+        Name:
+        <input type="text" className=" rounded-lg" name="name" />
+      </label>
+      <input type="submit" className="bg-cyan-100" value="Submit" />
+      </div>
+    </div>
+  );
+};
+export default Contact;
